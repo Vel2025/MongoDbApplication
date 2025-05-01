f65f1eed2041a282dd87a7e5d09ce4e42a696896
Vel2025 <velbalaiya@gmail.com>
2025-05-01 15:55:52 -0600

    server updated

--- server.mjs
+++ server.mjs
@@ -2,6 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import errorHandler from './middlewares/error.mjs';
 import connectDB from './db/conn.mjs';
+import countryRoutes from './routes/countries.mjs';
+import continentRoutes from './routes/continents.mjs';
+import languageRoutes from './routes/languages.mjs';
+import populateData from './data/sampleData.mjs';
 
 
 const app = express();
@@ -10,10 +14,15 @@ const PORT = process.env.PORT || 3001
 connectDB();
 
 
-
+//middleware
 app.use(express.json());
 //Routes
+app.use('/api/countries', countryRoutes);
+app.use('/api/continents', continentRoutes);
+app.use('/api/languages', languageRoutes);
 
+// Populate sample data on server start
+populateData();
 
 //Err Handling Middleware
 app.use(errorHandler);
